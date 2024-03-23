8cfb2aeeb1e5615c37aa738d2ba165f012d0ed29
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-23 20:56:54 +0530

    generate date

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,12 +1,19 @@
-import { GenerateDate } from './GenerateDate';
+import { GenerateDate } from './GenerateDate'; 
 
-const Calender = () => {
-    console.log(GenerateDate());
+const Calendar = () => {
+  const dates = GenerateDate(); 
+
   return (
     <div className='w-96 h-96'>     
-    
+      {dates.map(({ date}, index) => { 
+        return (
+          <div key={index}> 
+            <h1>{date.date()}</h1>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Calender
+export default Calendar;
