4b3de447a1ab3aca4719a75344749f4fc7f009a2
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-03-24 07:43:05 +0530

    styling

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,19 +1,32 @@
-import { GenerateDate } from './GenerateDate'; 
+import Cn from './Cn';
+import { GenerateDate } from './GenerateDate';
+
 
 const Calendar = () => {
-  const dates = GenerateDate(); 
+    const dates = GenerateDate();
+    const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 
-  return (
-    <div className='w-96 h-96'>     
-      {dates.map(({ date}, index) => { 
-        return (
-          <div key={index}> 
-            <h1>{date.date()}</h1>
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className='w-96 h-96'>
+            <div className='w-full grid grid-cols-7'>
+                {days.map((day,index)=>{
+                    return <h1 key={index}  className='h-14 grid place-content-center text-sm font-bold'>{day}</h1>
+                    
+                })}
+            </div>
+            <div className='w-full grid grid-cols-7'>
+                {dates.map(({ date,currentMonth,today }, index) => {
+                    return (
+                        <div key={index} className='h-14 border-t-2 grid place-content-center text-sm'>
+                            <h1 className={Cn(currentMonth?"":"text-gray-400")}>{date.date()}</h1>
+                        </div>
+                    );
+                })}
+            </div>
+
+        </div>
+
+    );
 }
 
 export default Calendar;
---
+++ src/components/Cn.jsx
@@ -0,0 +1,3 @@
+export default function Cn (...classes) {
+    return classes.filter(Boolean).join("")
+}
